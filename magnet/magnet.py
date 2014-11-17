1cb28b7e794ca187adc49b04026b401c74bfdd39
shanlihou <shanlihou@gmail.com>
2014-11-17 21:53:27 +0800

    add a magnet repo

---
+++ magnet/magnet.py
@@ -0,0 +1,30 @@
+import urllib2 
+import re
+import sys
+def getUrlList(urlPath):
+	response = urllib2.urlopen(urlPath)
+	html = response.read()
+	pattern = re.compile(r'http://www\.btspread\.com/magnet/detail/hash/[A-F0-9]+')
+	patFind = pattern.search(html)
+	if (patFind):
+		matchList = pattern.findall(html)
+		return matchList
+	return None
+
+def getMagnet(urlPath):
+	response = urllib2.urlopen(urlPath)
+	html = response.read()
+	pattern = re.compile(r'(magnet:\?xt=urn:btih:.+)" class=')
+	patFind = pattern.search(html)
+	if (patFind):
+		print patFind.group(1)
+		print '\n'
+
+def getAllMagnet(code):
+	List = getUrlList('http://www.btspread.com/search/' + code)
+	if (List != None):
+		for url in List:
+			getMagnet(url)
+
+if (len(sys.argv) == 2):
+	getAllMagnet(sys.argv[1])
