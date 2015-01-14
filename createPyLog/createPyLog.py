1fc26dd66b9a8d429e373444d9087a3ab0d18ec0
shanlihou <shanlihou@gmail.com>
2015-01-14 06:14:25 -0800

    add createPyLog

---
+++ createPyLog/createPyLog.py
@@ -0,0 +1,59 @@
+#listdir.py
+import os 
+import sys
+import re
+def countBrackets(strBrack):
+	count = 0
+	for i in strBrack:
+		if (i == '('):
+			count = count + 1
+		elif (i == ')'):
+			count = count - 1
+	return count
+
+
+def AddLogByDef(fileName, tagName):
+	pattern = re.compile(r'^(\s*)def\s*(\w+)\(')
+	patSpace = re.compile(r'^(\s*)\S')
+	fileRead = open(fileName, 'r')
+	fileWrite = open(fileName + '.new', 'w')
+	flag = -1
+	count = 0
+	for line in fileRead:
+		strFind = pattern.search(line)
+		if (strFind):
+			strWrite = 'print(\'' + fileName + ':' + strFind.group(2) + ' ' + str(count) + '\')'
+			print(line)
+			flag = countBrackets(line)
+			fileWrite.writelines(line)
+			continue
+		if (flag == 0):
+			strSpace = patSpace.search(line)
+			if (strSpace):
+				print(strSpace.group(1) + strWrite)
+				count = count + 1
+				fileWrite.writelines(strSpace.group(1) + strWrite  + '\n')
+			flag = -1
+		elif (flag > 0):
+			flag = flag + countBrackets(line)
+		count = count + 1
+		fileWrite.writelines(line)
+	fileRead.close()
+	fileWrite.close()
+	os.rename(fileName + '.new', fileName)
+	
+
+def listyoudir(level, path, tagName): 
+	pattern = re.compile(r'.+.py$')
+	for i in os.listdir(path): 
+		if os.path.isdir(path + '/' + i):
+			listyoudir(level+1, path + '/' + i, tagName)
+		else:
+			strFind = pattern.search(i)
+			if(strFind):
+				AddLogByDef(path + '/' + i, tagName)
+
+if (len(sys.argv) == 2 and sys.argv[1] == '-h'):
+	print('argv 1 is the path of you want to add log, and it will add the log of func for every java')
+elif (len(sys.argv) == 3):
+	listyoudir(0, sys.argv[1], sys.argv[2])
---
+++ createPyLog/createPyLog.sh
@@ -0,0 +1,2 @@
+python createPyLog.py /usr/local/lib/python3.4/dist-packages/bcloud shanlihou
+echo hello
