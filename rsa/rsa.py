db46b2814c6e34b700eb5c422754563a2f87c5a2
shanlihou <shanlihou@gmail.com>
2015-11-25 17:55:36 +0800

    nothing

--- rsa/rsa.py
+++ rsa/rsa.py
@@ -4,13 +4,49 @@ import traceback
 import urllib.request
 import sys 
 import http
+import json
+	
+def post(url, data):
+	header = {}
+	header["X-Bmob-Application-Id"] = "f959535a39bb9dec9ac4dab32e5961c5"
+	header["X-Bmob-REST-API-Key"] = "17342bb32e2df845778bb70391b1c4a6"
+	header["Content-Type"] = "application/json"
+
+	opener = urllib.request.build_opener()
+	opener.addheaders = [(k, v) for k,v in header.items()]
+	print(opener.addheaders)
+	req = ''
+	print(type(opener))
+	try:
+		req = opener.open(url, data=data)
+		print(req)
+		print(req.reason)
+	except OSError:
+		print(traceback.format_exc())
+	return req.read().decode()
+
+def addPubKey(pubkey):
+	url = "https://api.bmob.cn/1/classes/pubKey"
+	jsonData = {}
+	jsonData["pubKey"] = pubkey
+	sendData = json.dumps(jsonData);
+	print(sendData)
+	return post(url, sendData.encode())
+
 def createPem():
 	key = RSA.generate(1024)
-	print(key.publickey().exportKey('PEM'))
-	private = open('private.pem', 'wb')
+	print(key.publickey().exportKey('PEM').decode().replace('\n', ''))
+	ret = addPubKey(key.publickey().exportKey('PEM').decode())
+	jsonRet = json.loads(ret)
+	print('ret')
+	print(jsonRet)
+	print(jsonRet['objectId'])
+	objectId = jsonRet['objectId']
+
+	private = open(objectId + '.pri.pem', 'wb')
 	private.write(key.exportKey('PEM'))
 	private.close()
-	public = open('public.pem', 'wb')
+	public = open(objectId + '.pub.pem', 'wb')
 	public.write(key.publickey().exportKey('PEM'))
 	public.close()
 def importKey():
@@ -29,21 +65,8 @@ def decrypt(secret):
 	priN = PKCS1_v1_5.new(pri)
 	mess = priN.decrypt(secret, None)
 	print(mess)
-def post(url, data):
-	header = {}
-	header["X-Bmob-Application-Id"] = "f959535a39bb9dec9ac4dab32e5961c5"
-	header["X-Bmob-REST-API-Key"] = "17342bb32e2df845778bb70391b1c4a6"
-	header["Content-Type"] = "application/json"
-	print(header)
-
-	opener = urllib.request.build_opener()
-	opener.addheaders =  [(k, v) for k,v in header.items()]
-	req = ''
-	try:
-		req = opener.open(url)
-		print(req.read())
-	except OSError:
-		print(traceback.format_exc())
-post('https://api.bmob.cn/1/classes/pubkey', '{"pubkey":"1235215355"}'.encode())
-post('https://www.baidu.com', '{"pubkey":"1235215355"}'.encode())
-	
+if (len(sys.argv) == 2):
+	if (sys.argv[1] == '-c'):
+		createPem()
+	elif(sys.argv[1] == '-p'):
+		post("https://api.bmob.cn/1/classes/userInfo", '{"score":1337,"playerName":"Sean Plott","cheatMode":false}'.encode())
