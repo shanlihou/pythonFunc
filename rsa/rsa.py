63069f8191048757ad8791e31d7c4658e950c5e4
shanlihou <shanlihou@gmail.com>
2015-11-25 22:39:59 +0800

    nothing

--- rsa/rsa.py
+++ rsa/rsa.py
@@ -5,6 +5,7 @@ import urllib.request
 import sys 
 import http
 import json
+import base64
 	
 def post(url, data):
 	header = {}
@@ -24,6 +25,21 @@ def post(url, data):
 	except OSError:
 		print(traceback.format_exc())
 	return req.read().decode()
+def get(url):
+	header = {}
+	header["X-Bmob-Application-Id"] = "f959535a39bb9dec9ac4dab32e5961c5"
+	header["X-Bmob-REST-API-Key"] = "17342bb32e2df845778bb70391b1c4a6"
+	opener = urllib.request.build_opener()
+	opener.addheaders = [(k, v) for k,v in header.items()]
+	req = ''
+	try:
+		req = opener.open(url)
+		print(req)
+		print(req.reason)
+	except OSError:
+		print(traceback.format_exc())
+	return req.read().decode()
+
 
 def addPubKey(pubkey):
 	url = "https://api.bmob.cn/1/classes/pubKey"
@@ -49,9 +65,9 @@ def createPem():
 	public = open(objectId + '.pub.pem', 'wb')
 	public.write(key.publickey().exportKey('PEM'))
 	public.close()
-def importKey():
-	public = open('public.pem', 'rb')
-	private = open('private.pem', 'rb')
+def importKey(objId):
+	public = open(objId + '.pub.pem', 'rb')
+	private = open(objId + '.pri.pem', 'rb')
 	pub = RSA.importKey(public.read())
 	pri = RSA.importKey(private.read())
 	pubN = PKCS1_v1_5.new(pub)
@@ -59,14 +75,27 @@ def importKey():
 	secret = pubN.encrypt('410015216'.encode())
 	mess = priN.decrypt(secret, None)
 	print(mess)
-def decrypt(secret):
-	private = open('private.pem', 'rb')
+def decrypt(secret, objId):
+	private = open(objId + '.pri.pem', 'rb')
 	pri = RSA.importKey(private.read())
 	priN = PKCS1_v1_5.new(pri)
-	mess = priN.decrypt(secret, None)
-	print(mess)
+	mess = priN.decrypt(base64.decodestring(secret.encode()), None)
+	print(mess.decode())
+def getData():
+	ret = get("https://api.bmob.cn/1/classes/userInfo")
+	jsonRet = json.loads(ret)
+	print(jsonRet["results"])
+	results = jsonRet["results"]
+	for i in results:
+		decrypt(i['name'], i['key'])
+		decrypt(i['pass'], i['key'])
 if (len(sys.argv) == 2):
 	if (sys.argv[1] == '-c'):
 		createPem()
 	elif(sys.argv[1] == '-p'):
 		post("https://api.bmob.cn/1/classes/userInfo", '{"score":1337,"playerName":"Sean Plott","cheatMode":false}'.encode())
+	elif(sys.argv[1] == '-g'):
+		getData()
+elif(len(sys.argv) == 4):
+	if (sys.argv[1] == '-d'):
+		decrypt(sys.argv[2], sys.argv[3])
