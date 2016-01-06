20ddd3e74e0b1e22813f5a047871c500c1f181f8
shanlihou <shanlihou@gmail.com>
2016-01-06 16:07:25 +0800

    now could find func or var

---
+++ findFunc/findFunc.py
@@ -0,0 +1,43 @@
+#listdir.py
+import os 
+import sys
+import re
+funcSet = set()
+def findFunc(fileName, inner):
+	pattern = re.compile('\w*' + inner + '\w*', re.I)
+	global funcSet
+	fileRead = open(fileName, 'r')
+	for line in fileRead:
+		strFind = pattern.search(line)
+		if (strFind):
+			funcSet.add(strFind.group())
+		
+
+
+def findYourDir(level, path, inner, code):
+	filetype = re.compile(r'(\.py|\.java)$')
+	for i in os.listdir(path):
+		if os.path.isdir(path + '/' + i):
+			findYourDir(level + 1, path + '/' + i, inner, code)
+		else:
+			if (code == 'p'):
+				fileFind = filetype.search(i)
+				if (not fileFind):
+					continue
+			findFunc(path + '/' + i, inner)
+				
+
+def printSet():
+	global funcSet
+	print(len(funcSet))
+	for i in funcSet:
+		print(i)
+
+if (len(sys.argv) == 2 and sys.argv[1] == '-h'):
+	print('the first argv is path')
+	print('the second argv is inner which you want to search')
+if (len(sys.argv) == 3):
+	findYourDir(0, sys.argv[1], sys.argv[2], 'a')
+
+	printSet()
+
