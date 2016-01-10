f77c503fd548816ab2c554381516e3e96e72af44
shanlihou <shanlihou@gmail.com>
2016-01-10 15:27:10 +0800

    could add log

--- createSmaliLog/createSmaliLog.py
+++ createSmaliLog/createSmaliLog.py
@@ -1,16 +1,37 @@
 import sys
 import os
+import re
+import string
 def createSmaliLog(fileName):
+	reLocal = re.compile(r'.locals (\d+)')
+	fileName = fileName[:-1]
+	print('filename:' + fileName)
 	fileRead = open(fileName, 'r')
 	fileWrite = open(fileName + '.new', 'w')
+	nLocal = 0
+	count = 0
 	for line in fileRead:
+		localFind = reLocal.search(line)
+		if (localFind):
+			nLocal = string.atoi(localFind.group(1))
 		if (".prologue" in line):
-			fileWrite.writelines(line)
-			fileWrite.writelines("    const-string v0, \"" + fileName + '"\n')
-			fileWrite.writelines("    invoke-static {v0}, Lcrack;->log(Ljava/lang/String;)V\n")
+			if (nLocal != 0):
+				fileWrite.writelines(line)
+				count = count + 1
+				fileWrite.writelines("    const-string v0, \"" + fileName + ':' + str(count + 1) +  '"\n')
+				fileWrite.writelines("    invoke-static {v0}, Lcrack;->log(Ljava/lang/String;)V\n")
+				count = count + 2
 		else:
 			fileWrite.writelines(line)
+			count = count + 1
 	fileRead.close()
 	fileWrite.close()
 	os.rename(fileName + '.new', fileName)
-createSmaliLog(sys.argv[1])
+def openAllFile(fileName):
+	fileRead = open(fileName, 'r')
+	for line in fileRead:
+		print(line)
+		createSmaliLog(line)
+
+if (len(sys.argv) == 2):
+	openAllFile(sys.argv[1])
