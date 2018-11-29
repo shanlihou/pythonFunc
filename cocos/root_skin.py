d56992ad7cded6bd99a6c994fb84b099c008548e
shanlihou <shanlihou@gmail.com>
2018-11-29 20:00:05 +0800

    'auto'

---
+++ cocos/root_skin.py
@@ -0,0 +1,3 @@
+skin = [
+    ('body', (25, 91), 'gil-cuerpo.png', True, True, 0.5),
+  ]
---
+++ cocos/show_root_bone.py
@@ -0,0 +1,26 @@
+import cocos
+from cocos import skeleton
+
+# import the skeleton we have created
+import root_bone
+
+class TestLayer(cocos.layer.Layer):
+    def __init__(self):
+        super( TestLayer, self ).__init__()
+
+        x,y = cocos.director.director.get_window_size()
+
+        # create a ColorSkin for our skeleton
+        self.skin = skeleton.ColorSkin(root_bone.skeleton, (255,0,0,255))
+
+        # add the skin to the scene
+        self.add( self.skin )
+        x, y = cocos.director.director.get_window_size()
+        self.skin.position = x/2, y/2
+        
+
+if __name__ == '__main__':
+    cocos.director.director.init(width=800, height=100, caption="cici")
+    main_layer = TestLayer()
+    main_scene = cocos.scene.Scene(main_layer)
+    cocos.director.director.run(main_scene)
