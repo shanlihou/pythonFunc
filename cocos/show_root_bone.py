cbeaf53e69770b25d450a0284dc9e4a23803f3b6
shanlihou <shanlihou@gmail.com>
2018-12-03 20:00:05 +0800

    'auto'

--- cocos/show_root_bone.py
+++ cocos/show_root_bone.py
@@ -4,20 +4,21 @@ from cocos import skeleton
 # import the skeleton we have created
 import root_bone
 
+
 class TestLayer(cocos.layer.Layer):
     def __init__(self):
-        super( TestLayer, self ).__init__()
+        super(TestLayer, self).__init__()
 
-        x,y = cocos.director.director.get_window_size()
+        x, y = cocos.director.director.get_window_size()
 
         # create a ColorSkin for our skeleton
-        self.skin = skeleton.ColorSkin(root_bone.skeleton, (255,0,0,255))
+        self.skin = skeleton.ColorSkin(root_bone.skeleton, (255, 0, 0, 255))
 
         # add the skin to the scene
-        self.add( self.skin )
+        self.add(self.skin)
         x, y = cocos.director.director.get_window_size()
-        self.skin.position = x/2, y/2
-        
+        self.skin.position = x / 2, y / 2
+
 
 if __name__ == '__main__':
     cocos.director.director.init(width=800, height=100, caption="cici")
