c3983847d4dd17212446e94613774ce67c285af6
shanlihou <shanlihou@gmail.com>
2018-12-26 20:00:07 +0800

    'auto'

--- ftpBrowser/ftpBrowser.py
+++ ftpBrowser/ftpBrowser.py
@@ -1,10 +1,14 @@
 import ui
 from TabbedView import TabbedView
 
+
 class LoginView(ui.View):
     def __init__(self):
         self.button_height = 30
         self.viewDict = {}
+        self.addEdit('ip')
+        self.addEdit('name')
+        self.addEdit('pwd')
 
     def addEdit(self, name):
         edit = ui.TextField()
@@ -12,12 +16,14 @@ class LoginView(ui.View):
         edit.border_color = 'blue'
         self.add_subview(edit)
         self.viewDict[name] = edit
-    
-    
+
+    def layout(self):
+        for index, view in enumerate(self.viewDict.values()):
+            view.frame = (0, index * self.button_height, self.width,
+                          self.button_height)
+
+
 if __name__ == '__main__':
     v = TabbedView()
-    v.addtab(ui.View(name='red', bg_color='red'))
-    v.addtab(ui.View(bg_color='blue'))
-    v.addtab(ui.View(name='green', bg_color='green'))
-    v.addtab(ui.View(name='green', bg_color='green'))
+    v.addtab(LoginView())
     v.present()
