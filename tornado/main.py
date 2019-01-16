1faa9a9d69cfd49e9648086ce141dbb82afa1fba
shanlihou <shanlihou@gmail.com>
2019-01-16 20:00:07 +0800

    'auto'

--- tornado/main.py
+++ tornado/main.py
@@ -3,6 +3,7 @@ import tornado.ioloop
 import time
 import tornado.gen
 import sched
+import json
 # from tornado import gen
 from tornado.httpclient import AsyncHTTPClient
 import threading
@@ -19,7 +20,7 @@ def elasticRequest(uri, callback, method='GET', data=None, headers=None):
     )
     response = yield tornado.gen.Task(http_client.fetch, req)
     if response.error:
-        print('ckz: elasticRequest error:', response.error)
+        print('ckz: elasticRequest error:', response.error, uri)
         print(response.body)
     else:
         callback(response)
@@ -29,15 +30,43 @@ class FriendSearchMixin(object):
     headers = {'Content-Type': 'application/json'}
 
     def __init__(self):
-        self.ip = '192.168.16.67'
+        self.ip = '192.168.16.252'
         self.port = 9200
         self.uriBase = 'http://' + self.ip + ':' + str(self.port)
-        self.indexName = str(999) + '_friend'
+        self.indexName = str(10516) + '_friend'
+        self.typeName = 'Avatar'
+
+    def cat(self):
+        url = self.join(self.uriBase, '_cat/indices?v')
+
+        def func(resp):
+            print(resp.body.decode('utf-8'))
+        elasticRequest(url, func)
+
+    def setting(self):
+        data = '{"index.blocks.read_only_allow_delete": null}'
+        uri = self.join(self.uriBase, self.indexName, '_settings')
+
+        def func(resp):
+            print(resp.body)
+        print(uri)
+        elasticRequest(uri, func, 'PUT', data, self.headers)
+
+    def addAvatarInfo(self, name, gbId, id):
+        data = {'name': name,
+                'gbId': gbId}
+
+        data = json.dumps(data)
+        uri = self.join(self.uriBase, self.indexName, self.typeName, str(id))
+
+        def func(response):
+            print('add:', response.body)
+        elasticRequest(uri, func, 'PUT', data, self.headers)
 
     def initElastic(self):
         data = '''{
   "mappings": {
-    "Avatar": {
+    "%s": {
       "properties": {
         "name": {
           "type": "text",
@@ -50,21 +79,49 @@ class FriendSearchMixin(object):
       }
     }
   }
-}'''
+}''' % self.typeName
 
         def func(*args):
             print(args)
 
-        url = self.join(self.uriBase, self.indexName)
-        print(url)
-        elasticRequest(url, func, 'PUT', data, self.headers)
-        
+        uri = self.join(self.uriBase, self.indexName)
+        print(uri)
+        elasticRequest(uri, func, 'PUT', data, self.headers)
+
+    def searchAvatarName(self, name):
+        for i in name:
+            print(ord(i))
+        data = {'query': {'match': {'name': name}},
+                'size': 100
+                }
+        data = json.dumps(data)
+        uri = self.join(self.uriBase, self.indexName, self.typeName, '_search')
+
+        def func(resp):
+            body = resp.body.decode('utf-8')
+            jsonData = json.loads(body)
+            hits = jsonData['hits']['hits']
+            print(hits)
+            for data in hits:
+                print(data)
+                source = data['_source']
+                print(source)
+
+        elasticRequest(uri, func, 'POST', data, self.headers)
+
     def join(self, *args):
         return '/'.join(args)
+
+    def test(self):
+        self.initElastic()
+        # self.setting()
+        self.cat()
+        # self.addAvatarInfo('包青一天大旧人', 2299822224)
+        self.searchAvatarName('纳兰')
 
 
 if __name__ == "__main__":
     # test_run()
     fs = FriendSearchMixin()
-    fs.initElastic()
+    fs.test()
     tornado.ioloop.IOLoop.current().start()
