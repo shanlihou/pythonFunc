057645d6c0643eff11a3d83b3ccc5012d7793300
shanlihou <shanlihou@gmail.com>
2019-07-19 15:41:25 +0800

    up

--- SSR/SSR.py
+++ SSR/SSR.py
@@ -1,7 +1,24 @@
 import cfscrape
 from lxml import html
 import os
+import re
+import pickle
+import json
 # get ss from web
+
+
+def ping(ip):
+    cmd = os.popen('ping %s' % ip)
+    result = cmd.read()
+    pat = re.compile(r'\((\d+)\%.*\)')
+    find = pat.search(result)
+    if find:
+        result = int(find.groups()[0])
+        print(result)
+        if result < 5:
+            return True
+
+    return False
 
 
 def getFreeSS():
@@ -28,15 +45,62 @@ def getFreeSS():
             method = 'aes-256-cfb'
             tm = serverObjAttrs[startIndex + 4]
             country = serverObjAttrs[startIndex + 5]
-            sjson = {"server": ip, "server_port": int(
-                port), "password": passwd, "method": method, "remarks": ip + "-" + country}
+            sjson = {
+                "server": ip,
+                "server_port": int(port),
+                "password": passwd,
+                "method": method,
+                "remarks": ip + "-" + country
+            }
             servers.append(sjson)
         return servers
     else:
         return []
 
 
+class SSR(object):
+    def __init__(self):
+        pass
+
+    def save(self, result):
+        data = pickle.dumps(result)
+        with open('result.txt', 'wb') as fw:
+            fw.write(data)
+
+    def load(self):
+        with open('result.txt', 'rb') as fr:
+            result = fr.read()
+            return pickle.loads(result)
+
+    def toJson(self, result):
+        final = []
+        for data in result:
+            if not ping(data['server']):
+                continue
+
+            cfg = {
+                "server": data['server'],
+                "server_port": data['server_port'],
+                "password": data['password'],
+                "method": data['method'],
+                "plugin": "",
+                "plugin_opts": "",
+                "plugin_args": "",
+                "remarks": data["remarks"],
+                "timeout": 5
+            }
+            final.append(cfg)
+        with open('couldUse.txt', 'wb') as fw:
+            fw.write(pickle.dumps(final))
+
+        final = json.dumps(final, indent=4, separators=(',', ':'))
+        print(final)
+
+    def test(self):
+        result = self.load()
+        self.toJson(result)
+
+
 if __name__ == '__main__':
-    #print(getFreeSS())
-    ret = os.system('ping www.google.com')
-    print('ret:', ret)
+    ssr = SSR()
+    ssr.test()
