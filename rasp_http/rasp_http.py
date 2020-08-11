6f5c553966066bfe8f274a0019e8224bfb49f454
shanlihou <shanlihou@gmail.com>
2020-08-11 20:00:13 +0800

    'auto'

--- rasp_http/rasp_http.py
+++ rasp_http/rasp_http.py
@@ -4,6 +4,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from os import path
 from urllib.parse import urlparse
+import json
 
 curdir = path.dirname(path.realpath(__file__))
 sep = '/'
@@ -34,6 +35,23 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write('{"hello": 123}'.encode('utf-8'))
+
+    def do_POST(self):
+        print(self.headers)
+        print(self.command)
+        req_datas = self.rfile.read(
+            int(self.headers['content-length']))  # 重点在此步!
+        print(req_datas.decode())
+        data = {
+            'result_code': '2',
+            'result_desc': 'Success',
+            'timestamp': '',
+            'data': {'message_id': '25d55ad283aa400af464c76d713c07ad'}
+        }
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode('utf-8'))
 
     def _do_GET_(self):
         sendReply = False
