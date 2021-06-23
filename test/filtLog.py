f14055b8ddd2a13da688796764ebf036d6ffb29e
shanlihou <shanlihou@gmail.com>
2021-06-23 20:00:06 +0800

    'auto'

--- test/filtLog.py
+++ test/filtLog.py
@@ -36,17 +36,17 @@ def main():
 
 def out_test_0618():
     ori_file = r'E:\shLog\tmp\ori.log'
-    ori_base = r'C:\Users\Administrator\Documents\WXWork\1688851680425036\Cache\File\2021-06\202106181420_169558\0_9.150.94.241_202106181418_169558\9.150.94.241_data_home_user00_log_logger_baseapp.log'
+    ori_base = r'C:\Users\Administrator\Documents\WXWork\1688851680425036\Cache\File\2021-06\202106211720_171363\0_9.150.94.241_202106211713_171363\9.150.94.241_data_home_user00_log_baseapp_700001.log'
     filter_gbid = r'E:\shLog\tmp\ori.log.8444835983061208270'
     filter_avatar_id = r'E:\shLog\tmp\ori.log.96619'
-    filter_modify_failed = r'E:\shLog\tmp\base.18207'
+    filter_modify_failed = r'E:\shLog\tmp\base.454'
     #fw = open(filter_gbid, 'w')
     fw = open(filter_modify_failed, 'w')
 
 
     with open(ori_base) as fr:
         for line in fr:
-            if '18207' in line:
+            if 'line 454' in line:
                 fw.write(line)
 
 if __name__ == '__main__':
