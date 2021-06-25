8848af9e096887c8370ac4b7e415a8b4e07e0df8
shanlihou <shanlihou@gmail.com>
2021-06-25 20:00:09 +0800

    'auto'

--- test/filtLog.py
+++ test/filtLog.py
@@ -36,19 +36,19 @@ def main():
 
 def out_test_0618():
     ori_file = r'E:\shLog\tmp\ori.log'
-    ori_base = r'C:\Users\Administrator\Documents\WXWork\1688851680425036\Cache\File\2021-06\202106211720_171363\0_9.150.94.241_202106211713_171363\9.150.94.241_data_home_user00_log_logger_baseapp.log'
+    ori_base = r'C:\Users\Administrator\Documents\WXWork\1688851680425036\Cache\File\2021-06\202106211720_171363\0_9.150.94.241_202106211713_171363\9.150.94.241_data_home_user00_log_baseapp_700004.log'
     tmp_file = r'E:\shLog\tmp\db.8444835990201591397'
     filter_gbid = r'E:\shLog\tmp\ori.log.8444835983061208270'
     filter_avatar_id = r'E:\shLog\tmp\ori.log.96619'
-    filter_modify_failed = r'E:\shLog\tmp\base.29626'
+    filter_modify_failed = r'E:\shLog\tmp\base.loc'
     #fw = open(filter_gbid, 'w')
     fw = open(filter_modify_failed, 'w')
 
 
     with open(ori_base) as fr:
         for line in fr:
-            if 'Avatar(29626)' in line or '[Guild(28726)]' in line:
-                #print(line)
+            if 'ckz: _checkLocTileLeft pos invalid, 0, 2' in line:
+                print(line)
                 fw.write(line)
 
 
