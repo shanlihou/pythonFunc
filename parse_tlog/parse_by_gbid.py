7836983e86057cfc38402ba9a8506c2b3e2eb95c
shanlihou <shanlihou@gmail.com>
2021-10-14 20:00:08 +0800

    'auto'

--- parse_tlog/parse_by_gbid.py
+++ parse_tlog/parse_by_gbid.py
@@ -1,11 +1,24 @@
 import const
 import utils
+import time
 
 
 G_AVATAR_GBID = '8726031376526920939'
 FLOW_NAME = 'GuildFlow'
 WRITE_FILE_NAME = r'E:\shtlog\out\avatar.GuildFlow.log'
+RECORD_FILE = r'E:\shLog\tmp\records.txt'
 
+
+def print_records():
+    with open(RECORD_FILE) as fr:
+        for line in fr:
+            tups = line.split(' ')
+            tups = [i.strip() for i in tups if i.strip()]
+            _time = tups[-1]
+            _time = time.localtime(int(_time))
+            _time = time.strftime("%Y-%m-%d %H:%M:%S", _time)
+            tups[-1] = _time
+            print(tups)
 
 def main():
     with open(WRITE_FILE_NAME, 'w') as fw:
@@ -16,4 +29,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    print_records()
