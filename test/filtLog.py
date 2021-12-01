c0a4e1313bf46166fad7e84754144b7d5bbad730
shanlihou <shanlihou@gmail.com>
2021-12-01 20:00:09 +0800

    'auto'

--- test/filtLog.py
+++ test/filtLog.py
@@ -6,32 +6,22 @@ class State(object):
     avatar = 2
 
 def main():
-    gbId = ''
-    #pat = re.compile(r'Avatar\((\d+)\)\]_guildInit\: 5630366051649716225')
-    pat = re.compile(r'Avatar\((\d+)\)\]:_guildInit: 5630366051649716225')
-    idstr = ''
-    targetstr = '$$$$$$'
     fw = open(r'e:\shLog\tmp.log', 'w')
-    with open(r'E:\shLog\logger_baseapp.log.2021-03-30', encoding='utf-8') as fr:
-        while 1:
-            try:
-                line = fr.readline()
-                if not line:
-                    break
+    with open(r'E:\shLog\logger_baseapp.log', encoding='utf-8') as fr:
+        for line in fr:
+            if 'Guild(754)' in line:
+                fw.write(line)
+            elif 'Avatar(604)' in line:
+                if 'checkAchievementTrigger' in line:
+                    continue
+                elif 'pickUpItems' in line:
+                    continue
+                elif 'tlog' in line:
+                    continue
+                elif 'applyEndDigTreasure' in line:
+                    continue
 
-                find = pat.search(line)
-                if find:
-                    idstr = find.group(1)
-                    targetstr = f'[Avatar({idstr})]:_calcAct:'
-
-                if targetstr in line:
-                    tup = line.split(' ')
-                    now = int(tup[-2])
-                    last = int(tup[-1].strip())
-                    if now < last:
-                        print(line, now - last)
-            except UnicodeDecodeError as e:
-                pass
+                fw.write(line)
 
 
 def out_test_0618():
@@ -52,4 +42,4 @@ def out_test_0618():
 
 
 if __name__ == '__main__':
-    out_test_0618()
+    main()
