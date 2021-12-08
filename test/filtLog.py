ec8a7a81f78d9b03d707340d28112f4007594e2b
shanlihou <shanlihou@gmail.com>
2021-12-08 20:00:08 +0800

    'auto'

--- test/filtLog.py
+++ test/filtLog.py
@@ -9,35 +9,15 @@ def main():
     fw = open(r'e:\shLog\tmp.log', 'w')
     with open(r'E:\shLog\logger_baseapp.log', encoding='utf-8') as fr:
         for line in fr:
-            if 'Guild(754)' in line:
-                fw.write(line)
-            elif 'Avatar(604)' in line:
-                if 'checkAchievementTrigger' in line:
-                    continue
-                elif 'pickUpItems' in line:
-                    continue
-                elif 'tlog' in line:
-                    continue
-                elif 'applyEndDigTreasure' in line:
-                    continue
-
+            if '_sendMasAppInfoToClient' in line:
                 fw.write(line)
 
+            if '] entities enable' in line:
+                if '12-03' not in line:
+                    continue
 
-def out_test_0618():
-    ori_file = r'E:\shLog\tmp\ori.log'
-    ori_base = r'F:\shdownload\log\logger_baseapp.log.1-20210908-180010'
-    tmp_file = r'E:\shLog\tmp\db.8444835990201591397'
-    filter_gbid = r'E:\shLog\tmp\ori.log.8444835983061208270'
-    filter_avatar_id = r'E:\shLog\tmp\ori.log.96619'
-    final = r'F:\shdownload\log\1578921.log'
-    #fw = open(filter_gbid, 'w')
-    fw = open(final, 'w')
-
-
-    with open(ori_base) as fr:
-        for line in fr:
-            if '1578921' in line:
+                if 'Avatar' not in line:
+                    continue
                 fw.write(line)
 
 
