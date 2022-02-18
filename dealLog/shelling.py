286c875571df279241c24bbeedb037cf7ce9e135
shanlihou <shanlihou@gmail.com>
2022-02-18 20:00:13 +0800

    'auto'

---
+++ dealLog/shelling.py
@@ -0,0 +1,27 @@
+import re
+import shutil
+
+logPath = r'F:\shdownload\log\1530308_avatar.txt'
+
+
+def main():
+    fwname = '{}.{}'.format(logPath, 'new')
+    fw = open(fwname, 'w')
+    pat = re.compile(r'')
+
+    s_str = '"log": "'
+    e_str = '", "ip":'
+    with open(logPath) as fr:
+        for line in fr:
+            start = line.index(s_str)
+            end = line.index(e_str)
+            start += len(s_str)
+            data = line[start: end] + '\n'
+            fw.write(data)
+
+    fw.close()
+    shutil.move(fwname, logPath)
+
+
+if __name__ == '__main__':
+    main()
